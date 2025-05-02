30be21d786bba51b6f0fbe5418a680f75ad8f94b
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-05-02 20:53:10 -0300

    Adicionar configuração express do servidor e rota do livro

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require("express")
+const rotaLivro = require("./rotas/livro")
 const app = express()
+
+app.use(express.json())
+
+const port = 8000
+
+app.listen(port, () => {
+    console.log(`Escutando a porta ${port}`);
+})
