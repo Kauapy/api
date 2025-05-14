4a5a7737e815d83a5c69b0936985173f17daedef
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-05-14 18:41:38 -0300

    Refatora funções de controle de livros para melhorar validações e formatação

--- controladores/livros.js
+++ controladores/livros.js
@@ -1,5 +1,10 @@
-const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro, deletarLivroPorId } = require("../servicos/livros");
-
+const {
+  getTodosLivros,
+  getLivroPorId,
+  insereLivro,
+  modificaLivro,
+  deletarLivroPorId,
+} = require("../servicos/livros");
 
 function getLivros(req, res) {
   try {
@@ -13,12 +18,19 @@ function getLivros(req, res) {
 function getLivro(req, res) {
   try {
     const id = req.params.id;
+
+    if (id && Number(id)) {
+      return res.status(400).send("ID deve ser um número");
+    } else {
+      res.status(422);
+      res.send("ID inválido");
+    }
+
     const livro = getLivroPorId(id);
 
     if (!livro) {
       return res.status(404).send(`Livro com ID ${id} não encontrado`);
     }
-
     res.send(livro);
   } catch (error) {
     res.status(500).send(error.message);
@@ -28,34 +40,51 @@ function getLivro(req, res) {
 function postLivro(req, res) {
   try {
     const livroNovo = req.body;
-    insereLivro(livroNovo);
-    res.status(201);
-    res.send("Livro inserido com sucesso");
+
+    if(req.body.nome){
+      insereLivro(livroNovo);
+      res.status(201);
+      res.send("Livro inserido com sucesso");
+
+    }else{
+      res.status(422).send("O campo nome é obrigatório")
+    }
+
   } catch (error) {
     res.status(500).send(error.message);
   }
 }
 
-function patchLivro(req, res){
-  try{
-    const id = parseInt(req.params.id)
-    const body = req.body
-    modificaLivro(body, id)
-    res.send("Item modificado com sucesso")
-  }
-  catch(error){
+function patchLivro(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+
+    if (id && Number(id)) {
+      const body = req.body;
+      modificaLivro(body, id);
+      res.send("Item modificado com sucesso");
+    } else{
+      res.status(422).send("ID inválido");
+    }
+  } catch (error) {
     res.status(500).send(error.message);
   }
 }
 
-function deleteLivro(req, res){
-  try{
-    const id = parseInt(req.params.id)
-    deletarLivroPorId(id)
-    res.send("Item deletado com sucesso")
-  }catch(error){
-    res.status(500).send(error.message)
-}}
+function deleteLivro(req, res) {
+  try {
+    const id = parseInt(req.params.id);
+
+    if(id && Number(id)){
+      deletarLivroPorId(id);
+      res.send("Item deletado com sucesso");
+    }else{
+      res.status(422).send("ID inválido")
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
 module.exports = {
   getLivros,
